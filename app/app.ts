42b132d52519c4da11cb9c562fbd6512fb816ab5
Sanjeev Srithevan <sanjeevz1717@gmail.com>
2022-10-03 17:02:29 +0100

    Installed TS, configured project and configured webpack.

---
+++ app/app.ts
@@ -0,0 +1,30 @@
+function startGame() {
+    // Test comment
+
+    let playerName: string | undefined = getInputValue('playername');
+    logPlayer(playerName);
+
+    postScore(100, playerName);
+}
+
+function logPlayer(name: string = 'MultiMath Player'): void {
+    console.log(`New game starting for player: ${name}`);
+}
+
+function getInputValue(elementID: string): string | undefined {
+
+    const inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+
+    if (inputElement.value === '') {
+        return undefined;
+    } else {
+        return inputElement.value;
+    }
+}
+
+function postScore(score: number, playerName?: string): void {
+    const scoreElement: HTMLElement | null = document.getElementById("postedScores");
+    scoreElement!.innerText = `${score} - ${playerName}`;
+}
+
+document.getElementById("startGame")!.addEventListener('click', startGame);
