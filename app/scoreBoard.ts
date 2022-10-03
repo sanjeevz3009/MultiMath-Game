5c3b48bc436a182a5a2461fea4e8ed7d64a4a29a
Sanjeev Srithevan <sanjeevz1717@gmail.com>
2022-10-03 23:07:10 +0100

    Added type declaration files.

--- app/scoreBoard.ts
+++ app/scoreBoard.ts
@@ -1,12 +1,15 @@
 // <reference path="result.ts" />
 
 import { Result } from './result';
+import * as _ from 'lodash';
  
 export class Scoreboard {
     private results: Result[] = [];
 
     addResult(newResult: Result): void {
         this.results.push(newResult);
+        let allCapsName: string = _.upperCase(newResult.playerName);
+        console.log(`${allCapsName}: ${newResult.score}`);
     }
 
     updateScoreboard(): void {
