7ff0f1462f1d71307aacf33698fb3cae568698c0
Sanjeev Srithevan <sanjeevz1717@gmail.com>
2022-10-03 17:25:42 +0100

    Function types.

--- app/app.ts
+++ app/app.ts
@@ -4,6 +4,7 @@ function startGame() {
     logPlayer(playerName);
 
     postScore(80, playerName);
+    postScore(-5, playerName);
 }
 
 function logPlayer(name: string = 'MultiMath Player'): void {
@@ -23,8 +24,18 @@ function getInputValue(elementID: string): string | undefined {
 
 function postScore(score: number, playerName: string = "MultiMath Player"): void {
 
+    let logger: (value: string) => void;
+
+    if (score < 0) {
+        logger = logError;
+    } else {
+        logger = logMessage; 
+    }
+
     const scoreElement: HTMLElement | null = document.getElementById("postedScores");
     scoreElement!.innerText = `${score} - ${playerName}`;
+
+    logger(`Score: ${score}`);
 }
 
 document.getElementById("startGame")!.addEventListener('click', startGame);
@@ -36,4 +47,8 @@ document.getElementById("startGame")!.addEventListener('click', startGame);
 // Arrow function version of the traditional function above
 const logMessage = (message: string) => console.log(message);
 
-logMessage("Welcome to MultiMath");
+// logMessage("Welcome to MultiMath");
+
+function logError(err: string): void {
+    console.error(err);
+}
